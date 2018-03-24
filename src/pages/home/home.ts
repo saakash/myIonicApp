b52c8df56fc8f27874fbfa3706e76d92c9fe1ad8
aakash <ashrestha7@myseneca.ca>
2018-03-24 06:53:05 -0400

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Facebook,FacebookLoginResponse } from '@ionic-native/facebook';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  userData = null;
+  constructor(private facebook:Facebook) {
 
-  constructor(public navCtrl: NavController) {
-
+  }
+  logInWithFB(){
+    this.facebook.login(['email','public_profile']).then((response: FacebookLoginResponse)=>{
+      this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)',[]).then(profile => {
+        this.userData = {email:profile['email'],first_name:profile['first_name'],picture:profile['picture_large'] ['data'] ['url'],username:profile['name']};
+      })
+    })
   }
 
 }
