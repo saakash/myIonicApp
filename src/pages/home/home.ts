d08cd364c14db056bf71bd31cb06a00899c6b2b7
aakash <ashrestha7@myseneca.ca>
2018-04-04 22:28:22 -0400

    ionic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+//import { NavController } from 'ionic-angular';
 import { Facebook,FacebookLoginResponse } from '@ionic-native/facebook';
 @Component({
   selector: 'page-home',
@@ -14,6 +14,9 @@ export class HomePage {
     this.facebook.login(['email','public_profile']).then((response: FacebookLoginResponse)=>{
       this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)',[]).then(profile => {
         this.userData = {email:profile['email'],first_name:profile['first_name'],picture:profile['picture_large'] ['data'] ['url'],username:profile['name']};
+      })
+      .catch(function(err){
+        console.log(err);
       })
     })
   }
